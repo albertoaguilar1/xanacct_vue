7f38d0e4fbd83db5b39a5c3c847b7f1ed90c954c
jaguialrh <31484862+albertoaguilar1@users.noreply.github.com>
2023-08-22 10:00:19 -0600

    commit

---
+++ xana_1/src/utils/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import firebase from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBNzQ0UDzFKdtu_Sk-0VMhlrcGO5jnxaEQ",
+  authDomain: "xanacct-4fe63.firebaseapp.com",
+  projectId: "xanacct-4fe63",
+  storageBucket: "xanacct-4fe63.appspot.com",
+  messagingSenderId: "880102254260",
+  appId: "1:880102254260:web:df6f881d230a35f7fe59e1"
+};
+
+// Initialize 
+firebase.initializeApp(firebaseConfig);
